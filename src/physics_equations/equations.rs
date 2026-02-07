02cb97cf5b474791f9132ab2b30d7a7276c45be0
popiemon <kryo.over@gmail.com>
2026-02-07 11:42:02 +0900

    test_caseとrelative_eq

--- src/physics_equations/equations.rs
+++ src/physics_equations/equations.rs
@@ -18,13 +18,16 @@ pub fn conduction_band_density(effective_mass_coefficient: f64, temperature: f64
 mod tests {
     use super::*;
     use approx::relative_eq;
+    use test_case::test_case;
 
-    #[test]
-    fn test_conduction_band_density() {
-        let effective_mass_coefficient = 1.08;
-        let temp = 300.0;
-
+    #[test_case(1.08, 300.0, 2.81e25 ; "Silicon at 300K")]
+    #[test_case(0.26, 300.0, 6.02e24 ; "GaAs at 300K")]
+    fn test_conduction_band_density(
+        effective_mass_coefficient: f64,
+        temp: f64,
+        expected_density: f64,
+    ) {
         let density = conduction_band_density(effective_mass_coefficient, temp);
-        let _ = relative_eq!(density, 2.81e25, max_relative = 1e-3);
+        let _ = relative_eq!(density, expected_density, max_relative = 1e-3);
     }
 }
