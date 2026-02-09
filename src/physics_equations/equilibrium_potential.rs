db167d4d69fbf26188f068e87e780d59ef1e24ac
popiemon <kryo.over@gmail.com>
2026-02-09 15:36:22 +0900

    docstrings

--- src/physics_equations/equilibrium_potential.rs
+++ src/physics_equations/equilibrium_potential.rs
@@ -1,5 +1,16 @@
 use crate::constants::physics::*;
 
+/// Calculate the equilibrium potential for n-type semiconductor.
+///
+/// # Arguments
+///
+/// - `conduction_band_density` (`&f64`) - The effective density of states in the conduction band (in m^-3).
+/// - `donor_concentration` (`&f64`) - The donor concentration (in m^-3).
+/// - `temperature` (`&f64`) - The temperature (in K).
+///
+/// # Returns
+///
+/// - `f64` - The equilibrium potential (Ec - Ef) (in eV).
 pub fn equilibrium_potential_n_type(
     conduction_band_density: &f64,
     donor_concentration: &f64,
