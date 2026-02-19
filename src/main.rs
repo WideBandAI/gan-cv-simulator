7370bdadb3baabdd9b146ab0ec933543c3af69a1
popiemon <kryo.over@gmail.com>
2026-02-20 00:18:29 +0900

    delete solver

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 pub mod cli;
 pub mod constants;
-pub mod electrostatics;
 pub mod mesh_builder;
 pub mod physics_equations;
 pub mod utils;
 
 use crate::cli::configuration_builder::ConfigurationBuilder;
-use crate::electrostatics::solver::Solver;
 use crate::mesh_builder::mesh_builder as mb;
 
 fn main() {
@@ -15,16 +13,5 @@ fn main() {
     println!("{:#?}", config);
     let mesh_structure = mb::build(&config);
     println!("{:#?}", mesh_structure);
-    let mut solver = Solver::new(
-        mesh_structure,
-        1.0,
-        config.measurement.temperature.temperature,
-    );
-    solver.set_boundary_conditions(
-        config.measurement.voltage.start,
-        config.boundary_conditions.barrier_height,
-        config.boundary_conditions.ec_ef_bottom,
-    );
-    println!("{:#?}", solver);
     println!("Simulation complete.");
 }
