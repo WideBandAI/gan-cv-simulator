eb9b0b4d1e837bb10283a9d4de27776f9f8f69f9
popiemon <kryo.over@gmail.com>
2026-02-19 23:41:31 +0900

    test of donor activation

--- src/physics_equations/donor_activation.rs
+++ src/physics_equations/donor_activation.rs
@@ -24,3 +24,27 @@ pub fn ionized_donor_density(donor_density: f64, temperature: f64, potential: f6
         donor_density / (1.0 + 2.0 * (-potential * Q_ELECTRON / (K_BOLTZMANN * temperature)).exp());
     ion_nd
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::relative_eq;
+    use test_case::test_case;
+
+    #[test_case(1e22, 300.0, 1.0, 1e22 ; "high-potential")]
+    #[test_case(1e22, 300.0, 0.5, 1e22 ; "midium-potential")]
+    #[test_case(1e22, 300.0, 0.0, 3.333e21 ; "low-potential")]
+    fn test_ionized_donor_density(
+        donor_density: f64,
+        temperature: f64,
+        potential: f64,
+        expected_ionized_donor_density: f64,
+    ) {
+        let ionized_donor_density = ionized_donor_density(donor_density, temperature, potential);
+        assert!(relative_eq!(
+            ionized_donor_density,
+            expected_ionized_donor_density,
+            max_relative = 1e-3
+        ));
+    }
+}
