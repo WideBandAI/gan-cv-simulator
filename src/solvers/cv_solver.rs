e16b38a26a4b5058c5b4435b430dd2ac09ea06d0
popiemon <kryo.over@gmail.com>
2026-03-04 17:54:08 +0900

    fix profile at vg

--- src/solvers/cv_solver.rs
+++ src/solvers/cv_solver.rs
@@ -82,7 +82,10 @@ impl CVSolver {
     }
 
     fn solve_cv(&mut self, gate_voltage: f64) -> f64 {
-        _ = self.electron_density_at_vg(gate_voltage); // set potential profile at gate voltage
+        // set potential profile at gate voltage
+        self.set_gate_voltage(gate_voltage);
+        self.poisson_solver.solve_poisson();
+
         let electron_density_vg_plus_ac =
             self.electron_density_at_vg(gate_voltage + self.measurement.ac_voltage);
         let electron_density_vg_minus_ac =
