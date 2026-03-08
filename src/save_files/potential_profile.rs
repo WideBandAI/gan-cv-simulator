1fbc9cd134dbba1dc026d4c909bba9dccc5b72d5
popiemon <kryo.over@gmail.com>
2026-03-08 23:51:49 +0900

    interface

--- src/save_files/potential_profile.rs
+++ src/save_files/potential_profile.rs
@@ -1,5 +1,6 @@
 use crate::constants::physics::*;
 use crate::constants::units::*;
+use crate::mesh_builder::mesh_builder::IDX;
 use crate::mesh_builder::mesh_builder::{FixChargeDensity, MeshStructure};
 use crate::solvers::poisson_solver::Potential;
 use std::fs;
@@ -80,11 +81,6 @@ pub fn save_potential_profile(
     for idx in 0..profile.depth.len() {
         let layer_name = mesh_structure.name[idx].clone();
         let depth_nm = profile.depth[idx] * M_TO_NM;
-        let ec = profile.potential[idx] + mesh_structure.delta_conduction_band[idx];
-        let ev = ec - mesh_structure.bandgap_energy[idx];
-        let ns = profile.electron_density[idx] * M3_TO_CM3; // convert from 1/m^3 to 1/cm^3
-        let nd_plus = profile.ionized_donor_concentration[idx] * M3_TO_CM3; // convert from 1/m^3 to 1/cm^3
-        let nd = mesh_structure.donor_concentration[idx] * M3_TO_CM3; // convert from 1/m^3 to 1/cm^3
         let me = mesh_structure.mass_electron[idx] / M_ELECTRON;
         let epsilon_r = mesh_structure.permittivity[idx] / EPSILON_0;
         let fix_charge_bulk = match mesh_structure.fixcharge_density[idx] {
@@ -96,24 +92,76 @@ pub fn save_potential_profile(
             _ => 0.0,
         };
 
-        if writeln!(
-            file,
-            "{}, {:.3}, {:.3}, {:.3}, {:.3e}, {:.3e}, {:.3e}, {:.2}, {:.2}, {:.3e}, {:.3e}",
-            layer_name,
-            depth_nm,
-            ec,
-            ev,
-            ns,
-            nd_plus,
-            nd,
-            me,
-            epsilon_r,
-            fix_charge_bulk,
-            fix_charge_interface
-        )
-        .is_err()
-        {
-            return;
+        if matches!(mesh_structure.id[idx], IDX::Interface(_)) {
+            let ec1 = profile.potential[idx] + mesh_structure.delta_conduction_band[idx - 1];
+            let ev1 = ec1 - mesh_structure.bandgap_energy[idx - 1];
+            if writeln!(
+                file,
+                "{}, {:.3}, {:.3}, {:.3}, {:.3e}, {:.3e}, {:.3e}, {:.2}, {:.2}, {:.3e}, {:.3e}",
+                layer_name,
+                depth_nm,
+                ec1,
+                ev1,
+                0.0,
+                0.0,
+                0.0,
+                me,
+                epsilon_r,
+                fix_charge_bulk,
+                fix_charge_interface
+            )
+            .is_err()
+            {
+                return;
+            }
+
+            let ec2 = profile.potential[idx] + mesh_structure.delta_conduction_band[idx + 1];
+            let ev2 = ec2 - mesh_structure.bandgap_energy[idx + 1];
+            if writeln!(
+                file,
+                "{}, {:.3}, {:.3}, {:.3}, {:.3e}, {:.3e}, {:.3e}, {:.2}, {:.2}, {:.3e}, {:.3e}",
+                layer_name,
+                depth_nm,
+                ec2,
+                ev2,
+                0.0,
+                0.0,
+                0.0,
+                me,
+                epsilon_r,
+                fix_charge_bulk,
+                fix_charge_interface
+            )
+            .is_err()
+            {
+                return;
+            }
+        } else {
+            let ec = profile.potential[idx] + mesh_structure.delta_conduction_band[idx];
+            let ev = ec - mesh_structure.bandgap_energy[idx];
+            let ns = profile.electron_density[idx] * M3_TO_CM3; // convert
+            let nd_plus = profile.ionized_donor_concentration[idx] * M3_TO_CM3; // convert from 1/m^3 to 1/cm^3
+            let nd = mesh_structure.donor_concentration[idx] * M3_TO_CM3; // convert from 1/m^3 to 1/cm^3
+
+            if writeln!(
+                file,
+                "{}, {:.3}, {:.3}, {:.3}, {:.3e}, {:.3e}, {:.3e}, {:.2}, {:.2}, {:.3e}, {:.3e}",
+                layer_name,
+                depth_nm,
+                ec,
+                ev,
+                ns,
+                nd_plus,
+                nd,
+                me,
+                epsilon_r,
+                fix_charge_bulk,
+                fix_charge_interface
+            )
+            .is_err()
+            {
+                return;
+            }
         }
     }
 }
