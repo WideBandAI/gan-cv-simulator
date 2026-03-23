73b9eb4043d319fa7510f0fe1808b700a968cd8f
popiemon <kryo.over@gmail.com>
2026-03-24 01:18:34 +0900

    helper

--- src/config/interface_states.rs
+++ src/config/interface_states.rs
@@ -52,42 +52,11 @@ pub fn define_interface_states(
             false,
         );
         if has_continuous_traps {
-            let bandgap: f64 =
-                device_structure.bandgap_energy[i].min(device_structure.bandgap_energy[i + 1]);
-
-            let dit0: f64 = get_parsed_input_with_default_nonnegative(
-                &format!("Enter Dit0 (cm^-2) for interface {}: default is 1e12 ", i),
-                1e12,
-            );
-            let nssec: f64 = get_parsed_input_with_default_nonnegative(
-                &format!("Enter nssec for interface {}: default is 10 ", i),
-                10.0,
-            );
-            let nssev: f64 = get_parsed_input_with_default_nonnegative(
-                &format!("Enter nssev for interface {}: default is 10 ", i),
-                10.0,
-            );
-            let ecnl: f64 = get_parsed_input_with_default_nonnegative(
-                &format!(
-                    "Enter |Ec - Ecnl| (eV) for interface {}: default is 1.3 ",
-                    i
-                ),
-                1.3,
-            );
-            let nd: f64 = get_parsed_input_with_default_nonnegative(
-                &format!("Enter nd for interface {}: default is 3 ", i),
-                3.0,
-            );
-            let na: f64 = get_parsed_input_with_default_nonnegative(
-                &format!("Enter na for interface {}: default is 3 ", i),
-                3.0,
-            );
+            let params = configure_continuous_interface_states(i, device_structure);
             continuous_interface_states_config
                 .interface_id
                 .push(i as u32);
-            continuous_interface_states_config
-                .parameters
-                .push(DIGSModel::new(dit0, nssec, nssev, ecnl, nd, na, bandgap));
+            continuous_interface_states_config.parameters.push(params);
         }
 
         let has_discrete_traps: bool = get_parsed_input_with_default(
@@ -152,4 +121,78 @@ fn get_discrete_state_type() -> DiscreteStateType {
             _ => println!("Invalid input. Please enter 'd' or 'a'."),
         }
     }
+}
+
+fn configure_continuous_interface_states(
+    i: usize,
+    device_structure: &DeviceStructure,
+) -> DIGSModel {
+    let bandgap: f64 =
+        device_structure.bandgap_energy[i].min(device_structure.bandgap_energy[i + 1]);
+
+    let dit0: f64 = get_parsed_input_with_default_nonnegative(
+        &format!("Enter Dit0 (cm^-2) for interface {}: default is 1e12 ", i),
+        1e12,
+    );
+    let nssec: f64 = get_parsed_input_with_default_nonnegative(
+        &format!("Enter nssec for interface {}: default is 10 ", i),
+        10.0,
+    );
+    let nssev: f64 = get_parsed_input_with_default_nonnegative(
+        &format!("Enter nssev for interface {}: default is 10 ", i),
+        10.0,
+    );
+    let ecnl: f64 = get_parsed_input_with_default_nonnegative(
+        &format!(
+            "Enter |Ec - Ecnl| (eV) for interface {}: default is 1.3 ",
+            i
+        ),
+        1.3,
+    );
+    let nd: f64 = get_parsed_input_with_default_nonnegative(
+        &format!("Enter nd for interface {}: default is 3 ", i),
+        3.0,
+    );
+    let na: f64 = get_parsed_input_with_default_nonnegative(
+        &format!("Enter na for interface {}: default is 3 ", i),
+        3.0,
+    );
+    DIGSModel::new(dit0, nssec, nssev, ecnl, nd, na, bandgap)
+}
+
+fn configure_discrete_interface_states(i: usize) -> Vec<DiscreteModel> {
+    let num_discrete_traps: u32 = get_parsed_input_with_default_positiveint(
+        &format!(
+            "Enter the number of discrete traps for interface {}: default is 1 ",
+            i
+        ),
+        1,
+    );
+    let mut discrete_parameters = Vec::new();
+    for j in 0..num_discrete_traps {
+        let ditmax: f64 = get_parsed_input_with_default_nonnegative(
+            &format!(
+                "Enter Ditmax (cm^-2) for interface {} discrete trap {}: default is 1e12 ",
+                i, j
+            ),
+            1e12,
+        );
+        let ed: f64 = get_parsed_input_with_default_nonnegative(
+            &format!(
+                "Enter |Ec - Ed| (eV) for interface {} discrete trap {}: default is 0.5 ",
+                i, j
+            ),
+            0.5,
+        );
+        let fwhm: f64 = get_parsed_input_with_default_nonnegative(
+            &format!(
+                "Enter FWHM (eV) for interface {} discrete trap {}: default is 0.3 ",
+                i, j
+            ),
+            0.3,
+        );
+        let state_type: DiscreteStateType = get_discrete_state_type();
+        discrete_parameters.push(DiscreteModel::new(ditmax, ed, fwhm, state_type));
+    }
+    discrete_parameters
 }
